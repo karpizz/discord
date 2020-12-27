8226841bc86912e92eaf67566cc1f13d1713ba7e
karpizz <tyrant233@gmail.com>
2020-12-27 16:08:29 +0200

    first commit

---
+++ bot.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client({
+    partials: ['MESSAGE']
+});
+
+const BOT_PREFIX = '!';
+const MOD_ME_COMMAND = 'mode-me';
+
+client.on('ready', () => {
+    console.log('Bot is ready to go');
+})
+
+client.on('messageDelete', msg => {
+    msg.channel.send('Stop deleting sk blt!');
+})
+
+client.on('message', msg => {
+    if (msg.content === '!love') {
+        // msg.react('❤️');
+        msg.channel.send('❤️');
+    }
+
+    if (msg.content === `${BOT_PREFIX}${MOD_ME_COMMAND}`) {
+        // msg.channel.send('PONG BLEAT!')
+        // msg.member.roles.add('792749590273196072')
+        modUser(msg.member);
+        // msg.reply('Pong! yOPTA')
+    }
+})
+
+function modUser(member) {
+    member.roles.add('792749590273196072');
+}
+
+client.login(process.env.BOT_TOKEN);
+
